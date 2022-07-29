a527ee8f4f362514355eec151f3470b82aa24359
Topan98 <topangiovanniaiden@gmail.com>
2022-07-29 20:15:26 +0700

    project vue

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+
+<Navbar/>
+  <Banner/>
+
+<div class="container mt-4">
+  <div class="row">
+    <div class="c0l-6 col-sm-4 col-md-3" v-for="i in 10" :key="i">
+     <Furniture/>
+    </div> 
+    </div>
+   </div>
+
+</template>
+
+<script>
+import Banner from "./components/Banner.vue";
+import Furniture from "./components/Furniture.vue";
+import Navbar from "./components/Navbar.vue";
+
+export default {
+    name: "App",
+    data() {
+        return {
+            nama_website: "furniture"
+        };
+    },
+    components: {
+    Banner,
+    Furniture,
+    Navbar
+},
+mounted() {
+console.log("sudah di render nih")
+}
+}
+
+</script>
---
+++ src/components/Furniture.vue
@@ -0,0 +1,10 @@
+<template>
+<div class="card mb-2">
+  <img src="https://tse3.mm.bing.net/th?id=OIP.BONHpP_YOASThP-DQG2a2AHaEy&pid=Api&P=0" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>
+</template>
